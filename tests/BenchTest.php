8f33f3cf5fc3458d9f9dcc41e1e9378c1f23082d
Philipp Speck <philipp@typo.media>
2022-10-25 21:50:40 +0200

    phpbench added

---
+++ tests/BenchTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Typomedia\Tests\Ini;
+
+use PhpBench\Benchmark\Metadata\Annotations\Iterations;
+use Typomedia\Ini\Dumper;
+use Typomedia\Ini\Parser;
+
+/**
+ * Class BenchTest
+ */
+class BenchTest
+{
+    /**
+     * @var Parser
+     */
+    private $parser;
+
+    /**
+     * @var false|string
+     */
+    private $ini;
+
+    public function setUp()
+    {
+        $this->parser = new Parser();
+        $this->ini = file_get_contents('tests/Fixtures/Database.ini');
+    }
+
+    /**
+     * @BeforeMethods("setUp")
+     * @Iterations(100)
+     */
+    public function benchNativeParser()
+    {
+        parse_ini_string($this->ini, true);
+    }
+
+    /**
+     * @BeforeMethods("setUp")
+     * @Iterations(100)
+     */
+    public function benchIniParser()
+    {
+        $this->parser->parse($this->ini);
+    }
+}
